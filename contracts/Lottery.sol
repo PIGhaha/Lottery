0031647553f3966b4306b5282152af0a91afc1a6
PIGhaha <78994667+PIGhaha@users.noreply.github.com>
2021-07-06 23:13:01 +0800

    Update Lottery.sol

--- contracts/Lottery.sol
+++ contracts/Lottery.sol
@@ -5,7 +5,6 @@ import "@openzeppelin/contracts/utils/structs/EnumerableSet.sol";
 import "@openzeppelin/contracts/utils/Address.sol";
 import "@openzeppelin/contracts/utils/math/SafeMath.sol";
 import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
-import "@openzeppelin/contracts/utils/Strings.sol";
 
 import "./RandomNumberGenerator.sol";
 
